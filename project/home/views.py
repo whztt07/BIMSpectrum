7f4a57b3b171c6873a1821f534080372ab323994
Ling Ma <malingreal@gmail.com>
2015-11-28 06:33:46 +0000

    parse ifc and post to mongo

--- project/home/views.py
+++ project/home/views.py
@@ -31,9 +31,10 @@ __author__ = 'ling'
 from flask import render_template, Blueprint, request, flash, redirect, jsonify, url_for
 from flask.ext.stormpath import login_required,user
 import requests
-import os
+import os,json
 from project import app
 from base64 import b64encode
+from project import ifcopenshell as ifc
 ################
 #### config ####
 ################
@@ -53,23 +54,44 @@ home_blueprint = Blueprint(
 @login_required # pragma: no cover 
 def home():
     if app.config['TESTING'] == True:
+        user_id = "abcdef"
+        username = "lorinma"
         email = "malingreal@gmail.com"
-        username = "Ling"
     else:
-        email = user.email
         username = user.given_name
-    r = requests.get(app.config['MONGODB_URL']+"/file?where=email=='"+email+"'")
-    return render_template('index.html', username=username, items=r.json(), db_url=app.config['MONGODB_URL'])
+        email = user.email
+        user_id = user.get_id()[-22:]
+    description = "test"
+    
+################
+#### test ####
+################
+    project_id = ""
+    r = requests.get(app.config['MONGODB_URL']+"/project?user_id="+user_id)
+    project_items=r.json()
+    if len(project_items['_items'])==0:
+        r = requests.post(app.config['MONGODB_URL']+"/project", data = {"username":username,"email":email,"user_id":user_id,"description":description})
+        project_items=r.json()
+        project_id=project_items["_id"]
+    else:
+        project_info = project_items['_items'][0]
+        project_id =  project_info['_id']
+################
+#### test ####
+################
+    r = requests.get(app.config['MONGODB_URL']+"/file?project_id="+project_id)
+    return render_template('index.html', username=username, project_id=project_id, items=r.json(), db_url=app.config['MONGODB_URL'])
     
 @home_blueprint.route('/upload')  # pragma: no cover
 @login_required  # pragma: no cover
 def upload():
-    if app.config['TESTING'] == True:
-        email = "malingreal@gmail.com"
-        username = "Ling"
-    else:
-        email = user.email
-        username = user.given_name
+    headers = {'Content-Type': 'application/json'}
+    project_id="5658757ad737bd5ec6b49832"
+        
+################
+#### test ####
+################
+    project_id = request.args.get('project_id')
     url = request.args.get('url')
     url = url[:-1]+'1'
     r = requests.get(url)
@@ -77,8 +99,57 @@ def upload():
     path = os.path.join(app.config['APP_TEMP_FILE'], b64encode(filename)+".ifc")
     with open(path, 'wb') as f:
         f.write(r.content)
-    r = requests.post(app.config['MONGODB_URL']+"/file", data = {"username":username , "filename":filename, "email":email})
-    response_data=r.json()
-    os.remove(path)
-    return jsonify(filename=filename,fileid=response_data['_id'])
     
+#### test ####
+    # filename = "test.ifc"
+    # path = os.path.join(app.config['APP_TEMP_FILE'], "test.ifc")
+#### test ####
+
+################
+#### test ####
+################
+    r = requests.post(app.config['MONGODB_URL']+"/file", data = {"project_id":project_id , "filename":filename})
+    response_data=r.json()
+    file_id=response_data['_id']
+    
+#### test ####
+    # file_id = "5654bfa9d737bd7e94b37e6c"
+#### test ####
+    
+    file = ifc.open(path)
+    # get all the entities line_ids
+    num=file.wrapped_data.entity_names()
+    data=list()
+    
+    for i in num:
+        entity = file.wrapped_data.by_id(i)
+        entity_data = {"line_id":entity.id(),"type": entity.is_a(),"content": entity.__str__(),"file_id":file_id, "project_id":file_id}
+        for j in xrange(entity.__len__()):
+            try:
+                entity_data[entity.get_argument_name(j)]=entity.get_argument(j).id()
+            except:
+                import types
+                if isinstance(entity.get_argument(j), list):
+                    entity_data[entity.get_argument_name(j)]=list()
+                    for v in entity.get_argument(j):
+                        try:
+                            entity_data[entity.get_argument_name(j)].append(v.id())
+                        except:
+                            entity_data[entity.get_argument_name(j)].append(v)
+                else:
+                    entity_data[entity.get_argument_name(j)]=entity.get_argument(j)
+        data.append(entity_data)
+
+    r = requests.post(app.config['MONGODB_URL']+"/entity",headers=headers, data = json.dumps(data))
+    
+# #### test ####
+#     response_data=r.json()
+#     return json.dumps(response_data)
+# #### test ####
+
+    os.remove(path)
+    return jsonify(filename=filename,file_id=file_id)
+
+# if you are going to fetch repeatedly from name, you better construct a dictionary with the names as keys so get operations are O(1)
+def build_dict(seq, key):
+    return dict((d[key], dict(d, index=i)) for (i, d) in enumerate(seq))
